4e65660e827d32b4595bd4ee1c1a90c305526475
Your Name <you@example.com>
2025-12-26 02:39:18 -0900

    quiz

--- controllers/userController.js
+++ controllers/userController.js
@@ -231,9 +231,16 @@ export const toggleBlockUser = async (req, res) => {
 // --------------------------
 // LOGOUT USER
 // --------------------------
+
 export const logoutUser = async (req, res) => {
   try {
-    const user = await User.findById(req.user.id);
+    const { userId } = req.body;
+
+    if (!userId) {
+      return res.status(400).json({ message: "userId is required" });
+    }
+
+    const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -250,6 +257,7 @@ export const logoutUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
 
 
 // --------------------------
